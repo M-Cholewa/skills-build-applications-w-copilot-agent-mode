9a602718e1be440a1be624054b4aa1da317758f6
Mateusz Cholewa <50336216+M-Cholewa@users.noreply.github.com>
2025-04-08 12:05:55 +0000

    Update ALLOWED_HOSTS and base_url for deployment

--- octofit-tracker/backend/octofit_tracker/settings.py
+++ octofit-tracker/backend/octofit_tracker/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-oh1lxplb1samxfdxm@+dg@oa(ny(c_l=$0rh156gse6p4knl%&
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['fluffy-lamp-vqjjpwv4prjcwrr6-8000.app.github.dev', 'localhost', '127.0.0.1']
 
 
 # Application definition
