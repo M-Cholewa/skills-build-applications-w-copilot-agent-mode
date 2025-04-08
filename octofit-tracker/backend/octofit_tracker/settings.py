9509a9db19ba79d5eaf0cde0a40499b0ffc9bf3c
Mateusz Cholewa <50336216+M-Cholewa@users.noreply.github.com>
2025-04-08 11:58:47 +0000

    Update database settings for MongoDB connection

--- octofit-tracker/backend/octofit_tracker/settings.py
+++ octofit-tracker/backend/octofit_tracker/settings.py
@@ -74,7 +74,8 @@ WSGI_APPLICATION = "octofit_tracker.wsgi.application"
 
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
-
+# djongo
+# djongo
 DATABASES = {
     'default': {
         'ENGINE': 'djongo',
